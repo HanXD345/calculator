58e6cfbdf4725e01946e213c5ed9a812f93ce8b8
HanXD345 <manpreet345@yahoo.com>
2025-05-19 17:05:02 -0400

    Condense JS code

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,8 +6,8 @@ for (let button of numButtons) {
         resultsSection.textContent = resultsSection.textContent === "0" 
                                         ? event.target.textContent 
                                         : resultsSection.textContent.concat("", event.target.textContent);
-    })
-}
+    });
+};
 
 const resetButton = document.querySelector(".reset");
 
